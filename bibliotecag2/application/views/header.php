3ac32184b652905b797f6d903078fe9ee7e50664
fiorela19 <fiorelapm7@gmail.com>
2020-07-26 15:26:54 -0500

    subiendo bootstrap

--- bibliotecag2/application/views/header.php
+++ bibliotecag2/application/views/header.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap4/css/bootstrap.min.css">
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/bootstrap4/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="container">
