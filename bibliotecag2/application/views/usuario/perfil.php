b1dce7f53c6d536cdca77ca86d142b311f9b47ee
keilaticona <keydy_26_12@hotmail.com>
2020-07-28 18:56:18 -0500

    Aumentando boton Ir a Ejemplares

--- bibliotecag2/application/views/usuario/perfil.php
+++ bibliotecag2/application/views/usuario/perfil.php
@@ -1,4 +1,4 @@
 <h2>Perfil de Usuario</h2>
 <a  href="<?php echo base_url('usuario/formulario') ?>"> Ingresar Nuevo Libro</a>
-<a  href="<?php echo base_url('usuario/ejemplares') ?>"> Ir a Ejemplares</a>
+<a  href="<?php echo base_url('usuario/ejemplar') ?>"> Ir a Ejemplares</a>
            
