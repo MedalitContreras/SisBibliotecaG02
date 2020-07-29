3f02e68c616f4e20d763380472b71e793b8760b4
keilaticona <keydy_26_12@hotmail.com>
2020-07-28 19:44:43 -0500

    Ejemplares en vista de perfil.php

--- bibliotecag2/application/models/model_usuario.php
+++ bibliotecag2/application/models/model_usuario.php
@@ -3,7 +3,7 @@ class Model_usuario extends CI_Model
 {
     public function consultar()
     {
-        $query = $this->db->get('ejemplar', 10);
+        $query = $this->db->get('ejemplar', 30);
         return $query->result();
     }
     public function guardar($data)
