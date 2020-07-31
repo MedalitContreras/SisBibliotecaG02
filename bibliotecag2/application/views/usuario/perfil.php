def6a4048f30a9af16543081a34324e6079a900e
MedalitContreras <milagros.contreras.as@gmail.com>
2020-07-30 22:00:46 -0500

    cambio

--- bibliotecag2/application/views/usuario/perfil.php
+++ bibliotecag2/application/views/usuario/perfil.php
@@ -22,5 +22,13 @@
   
   </nav>
 
+<div class="modal-dialog text-center">
+<div class="container form-group">
+<h3>BIENVENIDO A BIBLIOTECA VIRTUAL</h3>
+<h4> Gracias por ingresar a nuestro sistema :) </h4>
+<h4> </h4>
+<img src="../imagenes/virtual.gif"width="300" height="150">
+</div>
+</div>
  
 
