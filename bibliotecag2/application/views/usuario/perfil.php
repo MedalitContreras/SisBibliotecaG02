72bd6234212641208deeff95d0b0bddd0e2ca614
keilaticona <keydy_26_12@hotmail.com>
2020-07-30 19:34:24 -0500

    Inicio

--- bibliotecag2/application/views/usuario/perfil.php
+++ bibliotecag2/application/views/usuario/perfil.php
@@ -1,7 +1,7 @@
 <h2 align="center" class="display-4">Biblioteca Virtual</h2>
 <div class="container">
-<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;"> 
+ 
   <a  href="<?php echo base_url('usuario/formulario') ?>" class="navbar-brand" href="#"><i class="fas fa-book"></i> INGRESAR NUEVO LIBRO</a> 
 
   <a  href="<?php echo base_url('usuario/ejemplar') ?>"  class="navbar-brand" href="#"><i class="fas fa-book-open"></i> EJEMPLARES </a>
@@ -12,7 +12,7 @@
 
   <a href="<?php echo base_url('Login/salir'); ?>" class="navbar-brand" href="#"><i class="fas fa-sign-out-alt"></i> SALIR</a>
   
-  </button>
+  </nav>
 
  
 
