5481329bb1c6a9d076c33b31aa9af47ea02a59ed
MedalitContreras <milagros.contreras.as@gmail.com>
2020-10-16 07:48:17 -0500

    r

--- application/views/login/registro.php
+++ application/views/login/registro.php
@@ -1,4 +1,11 @@
 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 <title>Registrarse</title>
 <div></div>
 <h2 align="center" class="display-6"><p class="alert alert-secondary">Ingrese datos</p></h2>
@@ -82,7 +89,7 @@
             </div>
             <div class="form-group">
                 <td colspan="2"><input type="submit"  class="btn btn-outline-dark" value="Ingresar"></td>
-                <a  href="<?php echo base_url('usuario/regresar') ?>" type="submit" class="btn btn-outline-danger" ><i class="fas fa-user-times"></i> Cancelar</a> 
+                <a  href="<?php echo base_url('login') ?>" type="submit" class="btn btn-outline-danger" ><i class="fas fa-user-times"></i> Cancelar</a> 
             </div>
             </div>
 
